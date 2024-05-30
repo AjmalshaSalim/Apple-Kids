f35524b53c54829559c7ba748106adcc571bb906
janna <jannakondeth5@gmail.com>
2024-05-30 15:44:57 +0530

    make contact page responsive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,55 +12,55 @@ function Contact() {
        <div className='flex flex-col justify-center items-center space-y-4 p-6 '>
   <div className='flex flex-col sm:items-start w-full max-w-md items-center'>
     <h1 className='text-xl font-semibold text-pink-500 mb-1'>Our Location</h1>
-    <p className='text-base text-gray-600'>Kazhakutam, Thiruvananthapuram</p>
+    <p className='text-base text-gray-600'>National Nagar,Kallattumukku,Manacuad P.O,Trivandrum-09</p>
   </div>
   <div className='flex flex-col sm:items-start w-full max-w-md items-center'>
     <h1 className='text-xl font-semibold text-pink-500 mb-1'>Phone Number</h1>
-    <p className='text-base text-gray-600'>876564436</p>
+    <p className='text-base text-gray-600'>7736965145 | 8086029291</p>
   </div>
   <div className='flex flex-col sm:items-start w-full max-w-md items-center'>
     <h1 className='text-xl font-semibold text-pink-500 mb-1'>Email Address</h1>
     <p className='text-base text-gray-600'>abc@gmail.com</p>
   </div>
 </div>
-<div className='flex flex-col justify-center items-center space-y-4 p-6 w-full max-w-md'>
-  <div className='w-full'>
-    
-    <input 
-      type='text' 
-      placeholder='Enter Your Name' 
-      className='w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-purple-500' 
-    />
-  </div>
-  <div className='w-full'>
 
-    <input 
-      type='email' 
-      placeholder='Enter Your Email' 
-      className='w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-purple-500' 
-    />
+
+
+<div className='min-h-[500px] flex items-center justify-center'>
+  <div className='flex flex-col justify-center sm:items-start items-center space-y-4 p-6 w-full max-w-md'>
+    <div className='w-full'>
+      <input 
+        type='text' 
+        placeholder='Enter Your Name' 
+        className='w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-purple-500' 
+      />
+    </div>
+    <div className='w-full'>
+      <input 
+        type='email' 
+        placeholder='Enter Your Email' 
+        className='w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-purple-500' 
+      />
+    </div>
+    <div className='w-full'>
+      <input 
+        type='tel' 
+        placeholder='Enter Your Phone Number' 
+        className='w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-purple-500' 
+      />
+    </div>
+    <div className='w-full'>
+      <textarea 
+        placeholder='Enter Your Message' 
+        className='w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-purple-500 h-32' 
+      />
+    </div>
+    <button className='bg-cyan-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-lg'>
+      Send Message
+    </button>
   </div>
-  <div className='w-full'>
-   
-    <input 
-      type='tel' 
-      placeholder='Enter Your Phone Number' 
-      className='w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-purple-500' 
-    />
-  </div>
-  <div className='w-full'>
-   
-    <textarea 
-      placeholder='Enter Your Message' 
-      className='w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-purple-500 h-32' 
-    />
-  </div>
-  <button className='bg-cyan-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-lg'>
-              Send messege
-            </button>
 </div>
-
-      </div>
+</div>
 
 
 {/* { map} */}
