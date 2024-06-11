5cf1c4b2434cbb24801b474885e7fd53836aea6f
janna <jannakondeth5@gmail.com>
2024-06-11 18:05:00 +0530

    change cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,22 +3,26 @@ import Classes from './Classes'
 import Countup from './Countup'
 import Homecards from './Homecards'
 import HomeAbout from './HomeAbout'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div className='min-h-[600px]'>
+    <div className='min-h-[600px] '>
         {/* headeing section */}
 
-        <div className='min-h-[200px] flex flex-col justify-center items-center space-y-4 p-2 mt-2 mx-auto container'>
-  <button className='bg-purple-400 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-lg h-8 w-32'>
+        <div className='min-h-[200px] flex flex-col justify-center items-center space-y-4 p-4 mt-4 mx-auto container'>
+          <Link to='/contact' >
+          <button className='bg-[#379E3B] hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg h-8 w-32 font-fredoka'>
     Enroll Now
   </button>
-  <h1 className='text-3xl sm:text-4xl md:text-5xl font-bold font-serif text-center'>
+          </Link>
+  
+  <h1 className='text-2xl sm:text-5xl md:text-4xl font-bold font-serif text-center'>
     A day full of discoveries<br />and knowledge!
   </h1>
-  <p className='text-base sm:text-lg md:text-xl text-center'>
+  {/* <p className='text-base sm:text-lg md:text-xl text-center  font-league-spartan'>
      Explore Our Ongoing Classes!!!
-  </p>
+  </p> */}
 </div>
 
 
--- src/components/Homecards.jsx
+++ src/components/Homecards.jsx
@@ -5,25 +5,25 @@ function Homecards() {
   const cardData = [
     {
       id: 1,
-      imgSrc: "https://img.icons8.com/dusk/64/curriculum.png",
+      imgSrc: "https://img.icons8.com/ios/50/178E18/light-on--v1.png",
       title: "Active Learning",
       description: "This is a card description. Add some text here to describe the content of the card."
     },
     {
       id: 2,
-      imgSrc: "https://img.icons8.com/office/16/star--v1.png",
+      imgSrc: "https://img.icons8.com/ios/50/E63535/star--v1.png",
       title: "Expert Teachers",
       description: "This is a card description. Add some text here to describe the content of the card."
     },
     {
       id: 3,
-      imgSrc: "https://img.icons8.com/external-bearicons-outline-color-bearicons/64/external-Feedback-customer-review-bearicons-outline-color-bearicons.png",
+      imgSrc: "https://img.icons8.com/neon/96/thumb-up.png",
       title: "Strategy Teachers",
       description: "This is aStrategy Teachers card description. Add some text here to describe the content of the card."
     },
     {
       id: 4,
-      imgSrc: "https://img.icons8.com/glassmorphism/48/graduation-cap.png",
+      imgSrc: "https://img.icons8.com/fluency-systems-regular/48/35CCE6/confetti.png",
       title: "Full Day Programs",
       description: "This is a card description. Add some text here to describe the content of the card."
     }
@@ -36,10 +36,10 @@ function Homecards() {
         <div className=' min-h-[400px] flex md:flex-row flex-col gap-10 justify-center items-center   flex-wrap p-6'>
 
       {cardData.map((card,index) =>  (
-        <div  key={index} className='bg-white border-t-4 border-l-4 border-purple-300 rounded-lg w-52 h-64 p-4 shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out hover:bg-pink-100 group'>
+        <div  key={index} className='bg-white  border-purple-300 rounded-lg w-52 h-64 p-4 shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out  group'>
         <img width="48" height="48" src={card.imgSrc} alt={card.title} className='transition-transform duration-500 ease-in-out  group-hover:rotate-180' />
-        <h3 className='text-lg font-semibold mb-2 mt-4 text-purple-600 hover:text-purple-800 font-mono'>{card.title}</h3>
-        <p className='text-sm text-gray-700'>{card.description}</p>
+        <h3 className='text-lg font-semibold mb-2 mt-4 font-fredoka'>{card.title}</h3>
+        <p className='text-sm text-gray-700 font-league-spartan'>{card.description}</p>
       </div>
       ))}
         </div>
