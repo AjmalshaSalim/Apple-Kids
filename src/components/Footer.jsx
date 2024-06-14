6f98ba217531445d1c22ad1dead0420705a0c3fc
janna <jannakondeth5@gmail.com>
2024-06-14 15:08:04 +0530

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,92 +2,64 @@ import React from 'react'
 
 import bg from '../assets/images/aj1.png'
 import Footertop from './Footertop'
+import logo from '../assets/images/logoapp.png'
 
 function Footer() {
   return (
-    <>
+ <>
    <Footertop/>
 
 {/* second portion */}
 
-        <div className=' min-h-[600px] bg-no-repeat bg-cover ' style={{ backgroundImage: `url(${bg})` }}
+        <div className=' min-h-[600px] bg-no-repeat md:bg-cover bg-contain flex justify-start items-center overflow-hidden bg-[#F87253]  ' style={{ backgroundImage: `url(${bg})` }}
         >
-            {/* <div className=' grid sm:grid-cols-5 mx-auto container gap-2 mt-6 space-y-2'> */}
+         <div className=' grid md:grid-cols-4  mx-auto container'>
 
 
-     
-                 {/* <div className='flex flex-col space-y-6 justify-start items-center '>
-                <h1 className="text-white text-4xl  font-serif italic">
-        Lal Ninas
-      </h1>
-      <p className="text-white text-sm sm:text-base md:text-lg lg:text-xl">
+         <div className='flex flex-col space-y-6 justify-start items-center sm:p-2 p-4 '>
+               <img  src={logo} alt='logo' className='md:w-[100px] md:h-[100px] w-[50px] h-[50px]' />
+      <p className="text-white text-sm sm:text-base md:text-lg lg:text-xl font-league-spartan">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat libero illo beatae, aspernatur dignissimos quas consequuntur fuga quidem ad quisquam optio obcaecat.
       </p>
-                   <div className='flex '>
-                   <img width="30" height="30" src="https://img.icons8.com/bubbles/50/facebook-new.png" alt="facebook-new"/>
-                   <img width="30" height="30" src="https://img.icons8.com/bubbles/50/instagram-new--v2.png" alt="instagram-new--v2"/>
-                   <img width="30" height="30" src="https://img.icons8.com/bubbles/50/twitter-squared.png" alt="twitter-squared"/>
-                   <img width="30" height="30" src="https://img.icons8.com/bubbles/50/youtube-play.png" alt="youtube-play"/>
-                   </div>
+                   
                 </div> 
    
-
-                 <div className='flex flex-col space-y-6 justify-start items-center'>
-                <h1 className="text-3xl md:text-2xl font-bold text-white">Pages</h1>
-      <h3 className="text-lg md:text-md font-medium cursor-pointer text-white">Home</h3>
-      <h3 className="text-lg md:text-md font-medium cursor-pointer text-white">About Us</h3>
-      <h3 className="text-lg md:text-md font-medium cursor-pointer text-white">Contact</h3>
-      <h3 className="text-lg md:text-md font-medium  cursor-pointer text-white">News & Events</h3>
-      <h3 className="text-lg md:text-md font-medium  cursor-pointer text-white">Gallery</h3>
+             <div className='flex flex-col lg:space-y-6 space-y-3 justify-start items-center sm:p-2 p-4'>
+                <h1 className="text-3xl md:text-2xl font-bold text-white font-fredoka">Pages</h1>
+      <h3 className="text-lg md:text-md font-medium cursor-pointer text-white font-league-spartan">Home</h3>
+      <h3 className="text-lg md:text-md font-medium cursor-pointer text-white font-league-spartan">About Us</h3>
+      <h3 className="text-lg md:text-md font-medium cursor-pointer text-white font-league-spartan">Contact</h3>
+      <h3 className="text-lg md:text-md font-medium  cursor-pointer text-white font-league-spartan">News & Events</h3>
+      <h3 className="text-lg md:text-md font-medium  cursor-pointer text-white font-league-spartan">Gallery</h3>
+                </div>
+                <div className='flex flex-col lg:space-y-6 space-y-3 justify-start items-center sm:p-2 p-4'>
+                <h1 className="text-3xl md:text-2xl font-bold text-white  font-fredoka">courses</h1>
+      <h3 className="text-lg md:text-md font-medium cursor-pointer text-white font-league-spartan">Home</h3>
+      <h3 className="text-lg md:text-md font-medium cursor-pointer text-white font-league-spartan">About Us</h3>
+      <h3 className="text-lg md:text-md font-medium cursor-pointer text-white font-league-spartan">Contact</h3>
+      <h3 className="text-lg md:text-md font-medium  cursor-pointer text-white font-league-spartan">News & Events</h3>
+      <h3 className="text-lg md:text-md font-medium  cursor-pointer text-white font-league-spartan">Gallery</h3>
                 </div>
 
-
-      <div className='flex flex-col space-y-6 justify-start items-center'>
-                <h1 className="text-3xl md:text-2xl font-bold text-white">Programs</h1>
-                <h3 className="text-lg md:text-md font-medium cursor-pointer text-white">Home</h3>
-                <h3 className="text-lg md:text-md font-medium cursor-pointer text-white">About Us</h3>
-                <h3 className="text-lg md:text-md font-medium cursor-pointer text-white">Contact</h3>
-                <h3 className="text-lg md:text-md font-medium  cursor-pointer text-white">News & Events</h3>
-                <h3 className="text-lg md:text-md font-medium  cursor-pointer text-white">Gallery</h3>
-                </div>
-
-
-        <div className='flex flex-col space-y-6 justify-start items-center'>
-                <h1 className="text-3xl md:text-2xl font-bold text-white">Lessons</h1>
-                <h3 className="text-lg md:text-md font-medium cursor-pointer text-white">Courses</h3>
-                <h3 className="text-lg md:text-md font-medium cursor-pointer text-white">Classes</h3>
-                <h3 className="text-lg md:text-md font-medium cursor-pointer text-white">Groups</h3>
-                
-                </div>
-
-
-                <div className='flex flex-col space-y-6 justify-start sm:items-start items-center'>
-                <h1 className="text-3xl md:text-2xl font-bold text-white">Information</h1>
+                <div className='flex flex-col lg:space-y-6  space-y-3 justify-start items-center sm:p-2 p-4'>
+                <h1 className="text-3xl md:text-2xl font-bold text-white  font-fredoka">Information</h1>
                 <div className="flex items-center">
       <span><img width="16" height="16" src="https://img.icons8.com/tiny-glyph/16/FFFFFF/address.png" alt="address"/></span>
-      <h3 className="text-lg md:text-md font-medium cursor-pointer text-white ml-2 ">Kazhakutam,Trivandrum</h3>
+      <h3 className="text-lg md:text-md font-medium cursor-pointer text-white ml-2 font-league-spartan ">Kazhakutam,Trivandrum</h3>
     </div> 
     <div className="flex items-center">
       <span><img width="16" height="16" src="https://img.icons8.com/tiny-glyph/16/FFFFFF/email.png" alt="email"/></span>
-      <h3 className="text-lg md:text-md font-medium cursor-pointer text-white ml-2 ">abc@gmail.com</h3>
+      <h3 className="text-lg md:text-md font-medium cursor-pointer text-white ml-2 font-league-spartan ">abc@gmail.com</h3>
     </div> 
-               
-    <div className="flex items-center">
-      <span><img width="16" height="16" src="https://img.icons8.com/tiny-glyph/16/FFFFFF/ringer-volume.png" alt="ringer-volume"/></span>
-      <h3 className="text-lg md:text-md font-medium cursor-pointer text-white ml-2">+91 7552345617</h3>
-    </div> 
-                
-                </div>
+    </div>
+              
 
-            </div>
 
-            <div className='flex  sm:justify-start items-center justify-center sm:p-0 p-4 mx-auto container ' >
-               <p className='text-white sm:text-lg text-md'>a2zalphabetsolutionz@2024 -- This is a Sample Website</p>
-            </div> */} 
+         </div>
         </div>
 
-   
-    </>
+        </>    
+      
   )
 }
 
