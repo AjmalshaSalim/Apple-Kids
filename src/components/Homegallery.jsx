cf7cda428e04c588ff0cf3ac4400a368f22e7dd5
janna <jannakondeth5@gmail.com>
2024-06-18 11:13:15 +0530

    fetch homegallery

--- src/components/Homegallery.jsx
+++ src/components/Homegallery.jsx
@@ -3,41 +3,46 @@ import { Link } from 'react-router-dom'
 import aj22 from '../assets/images/aj22.png'
 import api from '../api/api'
 
-const galleryItems = [
-    {
-      id: 1,
-      image: 'https://images.pexels.com/photos/296302/pexels-photo-296302.jpeg?auto=compress&cs=tinysrgb&w=600',
-      alt: 'Gallery Image 1',
-      category: 'office'
-    },
-    {
-      id: 2,
-      image: 'https://images.pexels.com/photos/273062/pexels-photo-273062.jpeg?auto=compress&cs=tinysrgb&w=600',
-      alt: 'Gallery Image 2',
-      category: 'playing'
-    },
-    {
-      id: 3,
-      image: 'https://images.pexels.com/photos/2406271/pexels-photo-2406271.jpeg?auto=compress&cs=tinysrgb&w=600',
-      alt: 'Gallery Image 3',
-      category: 'sitting'
-    },
-    {
-      id: 4,
-      image: 'https://images.pexels.com/photos/1154180/pexels-photo-1154180.jpeg?auto=compress&cs=tinysrgb&w=600',
-      alt: 'Gallery Image 4',
-      category: 'office'
-    },
-]
+// const galleryItems = [
+//     {
+//       id: 1,
+//       image: 'https://images.pexels.com/photos/296302/pexels-photo-296302.jpeg?auto=compress&cs=tinysrgb&w=600',
+//       alt: 'Gallery Image 1',
+//       category: 'office'
+//     },
+//     {
+//       id: 2,
+//       image: 'https://images.pexels.com/photos/273062/pexels-photo-273062.jpeg?auto=compress&cs=tinysrgb&w=600',
+//       alt: 'Gallery Image 2',
+//       category: 'playing'
+//     },
+//     {
+//       id: 3,
+//       image: 'https://images.pexels.com/photos/2406271/pexels-photo-2406271.jpeg?auto=compress&cs=tinysrgb&w=600',
+//       alt: 'Gallery Image 3',
+//       category: 'sitting'
+//     },
+//     {
+//       id: 4,
+//       image: 'https://images.pexels.com/photos/1154180/pexels-photo-1154180.jpeg?auto=compress&cs=tinysrgb&w=600',
+//       alt: 'Gallery Image 4',
+//       category: 'office'
+//     },
+// ]
 
 function Homegallery() {
-  const[data,setData]=useState()
+  const[data,setData]=useState([])
+
   useEffect(()=>{
      const fetchImage=async()=>{
-          const response=await api.get('/gallery')
-          const res=response.data.gallery
-          const resImg=res.slice(0,4)
-          setData(resImg)
+
+         try {
+           const response=await api.get('/gallery')
+           console.log(response.data.photos)
+           setData(response.data.photos.slice(0,4))
+         } catch (error) {
+          console.log(error)
+         }
      }
      fetchImage()
   },[])
@@ -57,7 +62,7 @@ function Homegallery() {
       <div className="container mx-auto px-4">
        
         <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2  xl:grid-cols-4">
-          {galleryItems.map((item) => (
+          {data.map((item) => (
             <div key={item.id} className="bg-white p-4 rounded-lg shadow-lg  transform transition-transform duration-300 hover:scale-105">
               <img
                 src={item.image}
