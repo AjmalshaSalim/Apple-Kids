85b0d773c2906580b0fc407cc97b32e2b2ea2fb7
janna <jannakondeth5@gmail.com>
2024-06-20 10:12:35 +0530

    change countup

--- src/components/Countup.jsx
+++ src/components/Countup.jsx
@@ -12,7 +12,7 @@ function Countup() {
         triggerOnce: true,
         threshold: 0.5,
       });
-      const { ref: experienceRef, inView: experienceInView } = useInView({
+      const { ref: courseRef, inView: courseInView } = useInView({
         triggerOnce: true,
         threshold: 0.5,
       });
@@ -36,16 +36,16 @@ function Countup() {
       <h2 className='text-2xl font-semibold text-white font-fredoka'>students</h2>
     </div>
     <div ref={facilityRef} className='flex flex-col space-y-4 justify-center items-center m-4'>
-      {facilityInView && <CountUp end={20} duration={2} className='text-6xl font-extrabold text-white font-fredoka' />}
-      <h2 className='text-2xl font-semibold text-white font-fredoka '>services</h2>
+      {facilityInView && <CountUp end={8} duration={2} suffix='+' className='text-6xl font-extrabold text-white font-fredoka' />}
+      <h2 className='text-2xl font-semibold text-white font-fredoka '>Activities</h2>
     </div>
     <div ref={staffRef} className='flex flex-col space-y-4 justify-center items-center m-4'>
-      {staffInView && <CountUp end={8} duration={2} className='text-6xl font-extrabold text-white font-fredoka ' />}
-      <h2 className='text-2xl font-semibold text-white  font-fredoka'>Staff</h2>
+      {staffInView && <CountUp end={5} duration={2} suffix='+' className='text-6xl font-extrabold text-white font-fredoka ' />}
+      <h2 className='text-2xl font-semibold text-white  font-fredoka'>Staffs</h2>
     </div>
-    <div ref={experienceRef} className='flex flex-col space-y-4 justify-center items-center m-4'>
-      {experienceInView && <CountUp end={5} duration={2} className='text-6xl font-extrabold text-white font-fredoka ' />}
-      <h2 className='text-2xl font-semibold text-white font-fredoka '>Experience</h2>
+    <div ref={courseRef} className='flex flex-col space-y-4 justify-center items-center m-4'>
+      {courseInView && <CountUp end={6} duration={2} suffix='+' className='text-6xl font-extrabold text-white font-fredoka ' />}
+      <h2 className='text-2xl font-semibold text-white font-fredoka '>Courses</h2>
     </div>
   </div>
   </div>
