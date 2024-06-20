5c824c4eb31051972a7725f795e8018bfd86d646
janna <jannakondeth5@gmail.com>
2024-06-20 10:04:29 +0530

    change location

--- src/components/contact/Contactmap.jsx
+++ src/components/contact/Contactmap.jsx
@@ -6,7 +6,13 @@ function Contactmap() {
  
       <div className='grid lg:grid-cols-2 min-h-[600px]'>  
           <div className='flex items-center justify-center p-4 '>
-          <iframe className='h-[500px] sm:w-[500px] w-[300px]' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3945.4921144773434!2d76.87479147313799!3d8.548585296327833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05bf9bca82635f%3A0x72d8ed7960ffc184!2sTechnopark%20Phase%203!5e0!3m2!1sen!2sin!4v1717042920701!5m2!1sen!2sin"    allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3946.4205161416376!2d76.95031277313684!3d8.458422847663186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05bb232bb0dc01%3A0xddee0498b96fa2db!2sNational%20Nagar%2C%20Pavithra%20Nagar%2C%20Konchiravila%2C%20Paravankunnu%2C%20Thiruvananthapuram%2C%20Kerala%20695009!5e0!3m2!1sen!2sin!4v1718857845103!5m2!1sen!2sin"
+        className='h-[500px] sm:w-[500px] w-[300px]' 
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
           </div>
           <div className='flex justify-center lg:items-start items-center space-y-4 flex-col p-4'>
              <h1 className='font-fredoka text-xl sm:text-3xl md:text-4xl lg:text-5xl font-bold '>Contact Information</h1>
